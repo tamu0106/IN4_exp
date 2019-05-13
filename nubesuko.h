c056d7c2f3211da7473e9c03c25693cc239d1ce4
tamu0106 <45586916+tamu0106@users.noreply.github.com>
2019-05-13 16:34:34 +0900

    いっけなーい！遅刻遅刻！

---
+++ nubesuko.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <netpbm/pgm.h>
+
+gray **make_filter(int size, int type);
+gray **filterling(int **image, int rows, int cols, int filter_size, int **weight);
+gray **malloc_matrix(int rows, int cols);
+void free_matrix(int **matrix, int size);
