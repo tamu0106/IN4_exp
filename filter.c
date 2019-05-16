fe5aff1951f6ea3081aeef272e030267dc3dc258
Ryousuke <s1600727@sendai-nct.jp>
2019-05-16 17:45:28 +0900

    double 化

--- filter.c
+++ filter.c
@@ -1,6 +1,6 @@
 #include "nubesuko.h"
 
-gray **filterling(int **image, int cols, int rows, int filter_size, double **weight)
+double **filterling(int **image, int cols, int rows, int filter_size, double **weight)
 {
 
 	gray **filtered_img;
@@ -35,34 +35,6 @@ gray **filterling(int **image, int cols, int rows, int filter_size, double **wei
 	}
 
 	printf("uo2\n");
-	//nube = max_value / 255;
-
-	printf("uo3\n");
-	//正規化
-	for (y = 1; y < cols - 1; y++)
-	{
-		for (x = 1; x < rows - 1; x++)
-		{
-			/*
-			pixcel_value = 0.0;
-			for (i = -1; i < filter_size - 1; i++)
-			{
-				for (j = -1; j < filter_size - 1; j++)
-				{
-					/*
-					pixcel_value = pixcel_value + weight[i + 1][j + 1] * image[y + i][x + j];
-					pixcel_value = abs((int)pixcel_value);
-					pixcel_value *= nube;
-					if (pixcel_value > 255)
-					{
-						pixcel_value = 255;
-					}
-					filtered_img[y][x] = pixcel_value;
-				}
-			}
-			*/
-		}
-	}
 
 	return filtered_img;
 }
--- nubesuko.h
+++ nubesuko.h
@@ -4,7 +4,7 @@
 #include <netpbm/pgm.h>
 
 double **make_filter(int size, int type);
-gray **filterling(int **image, int rows, int cols, int filter_size, double **weight);
+double **filterling(int **image, int rows, int cols, int filter_size, double **weight);
 gray **malloc_matrix(int rows, int cols);
 void free_matrix(int **matrix, int size);
 gray **mendako(int **image, int cols, int rows, int filter_size, int pattern);
