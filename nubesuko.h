07cf2854c7471cc04d59968b003aaa650dc3a93b
Ryousuke <s1600727@sendai-nct.jp>
2019-05-16 16:39:36 +0900

    引数追加

--- nubesuko.h
+++ nubesuko.h
@@ -3,8 +3,8 @@
 #include <math.h>
 #include <pgm.h>
 
-double **make_filter(int size, int type);
+double **make_filter(int size, int type, int sub_type);
 gray **filterling(int **image, int rows, int cols, int filter_size, double **weight);
 gray **malloc_matrix(int rows, int cols);
 void free_matrix(int **matrix, int size);
-gray **mendako(int pattern)
+gray **mendako(int pattern);
