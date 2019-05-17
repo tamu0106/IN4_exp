e37a2cafc54453b01e95903cfb3d46783c215589
Ryousuke <s1600727@sendai-nct.jp>
2019-05-17 17:56:43 +0900

    sharpening

--- exp_c/code/fil_sharpening.c
+++ exp_c/code/fil_sharpening.c
@@ -21,6 +21,7 @@ gray **fil_sharpening(gray **image, int cols, int rows, int filter_size)
         {
             tmp[y][x] = (tmp[y][x] * multi) + correct;
             filtered_img[y][x] = (unsigned int)tmp[y][x];
+            filtered_img[y][x] = image[y][x] - filtered_img[y][x];
         }
     }
     return filtered_img;
