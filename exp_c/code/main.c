9822dacc3cd826f717efe63288cc6a7fb9bd6f3d
TRyousuke <s1600727@sendai-nct.jp>
2019-05-19 01:54:34 +0900

    Update main.c

--- exp_c/code/main.c
+++ exp_c/code/main.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
   FILE *file1, *file2;
   int pattern, size, cols, rows, i, j;
   double **array;
-  char filename[16];
+  char filename[32];
   gray **image, **answer, maxval;
 
   //画像の読み込み
@@ -14,39 +14,43 @@ int main(int argc, char *argv[])
 
   if (file1 == NULL)
   {
-    printf("ファイルが存在しません。\n");
-    return 0;
+    printf("Cannot open image file.\nexit.\n");
+    return -1;
   }
 
-  printf("file open\n");
+  printf("Log:file open\n");
 
   image = pgm_readpgm(file1, &cols, &rows, &maxval);
-  printf("read pbm\n");
+  printf("Log:read pbm\n");
   fclose(file1);
 
   //サイズの入力
-  printf("サイズを入力してください。\n");
+  printf("Filter size?\n>>");
   scanf("%d", &size);
 
   //フィルタ選択
-  printf("フィルタ処理を選択してください。\n");
+  printf("Select Filter Type.\n");
   printf("0.Smoothing_filter\n");
   printf("8.Differential_filter\n");
+  printf("9.Laplacian_filter\n");
   printf("16.Sharpening_filter\n");
+  printf(">>");
 
   scanf("%d", &pattern);
 
   //フィルタ処理
   answer = mendako(image, cols, rows, size, pattern);
 
-  printf("1.ファイルに保存\n");
-  printf("2.画面に表示\n");
+  printf("Select output.\n");
+  printf("1.Write File\n");
+  printf("2.Output Window\n");
+  printf(">>");
 
   scanf("%d", &pattern);
 
   if (pattern == 1)
   {
-    printf("ファイル名を入力してください\n");
+    printf("Filename?\n>>");
     scanf("%s", filename);
 
     file2 = fopen(filename, "wb");
