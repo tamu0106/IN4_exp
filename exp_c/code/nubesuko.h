db9fb37299dc1d502c58437d6d583a36e75308c8
Ryousuke <s1600727@sendai-nct.jp>
2019-06-10 15:53:09 +0900

    fix

--- exp_c/code/nubesuko.h
+++ exp_c/code/nubesuko.h
@@ -6,6 +6,7 @@
 gray **filter_processing(gray **image, int cols, int rows, int filter_size, int pattern);
 
 gray **fil_ave(gray **image, int cols, int rows, int filter_size);
+gray **fil_ave_weighted(gray **image, int cols, int rows, int filter_size);
 gray **fil_dif_prew(gray **image, int cols, int rows, int filter_size);
 gray **fil_dif_lap(gray **image, int cols, int rows, int filter_size);
 gray **fil_sharpening(gray **image, int cols, int rows, int filter_size, int mode);
